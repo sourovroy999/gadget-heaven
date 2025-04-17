eadf2a03d28a90d1145f8a9b2a1d78170d23d3f1
sourovroy999 <sourovroy3050@gmail.com>
2025-04-18 00:25:47 +0600

    actually the cards are responsive

--- src/Components/Gadget.jsx
+++ src/Components/Gadget.jsx
@@ -6,21 +6,22 @@ const Gadget = ({singleGadget}) => {
 
     return (
         <>
-        <div className="card  bg-base-100 shadow-sm ">
-  <figure className="px-5 pt-5">
-    <img 
-      src={product_image}
-      alt="Shoes"
-      className="rounded-xl border w-48 h-32 object-cover" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{product_title}</h2>
-    <p>Price:{price} $</p>
     
-      <Link to='details'  className="btn btn-outline hover:bg-fuchsia-50 rounded-3xl text-[#9538E2]">View Details</Link>
-    
-  </div>
-        </div>
+    <div className="card w-[220px]  bg-base-100 shadow-sm ">
+<figure className="px-5 pt-5">
+  <img 
+    src={product_image}
+    alt="Shoes"
+    className="rounded-xl border w-48 h-32 object-cover" />
+</figure>
+<div className="card-body">
+  <h2 className="card-title">{product_title}</h2>
+  <p>Price:{price} $</p>
+  
+    <Link to='details'  className="btn btn-outline hover:bg-fuchsia-50 rounded-3xl text-[#9538E2]">View Details</Link>
+  
+</div>
+      </div>
         </>
     );
 };
--- src/Components/Gadgets.jsx
+++ src/Components/Gadgets.jsx
@@ -14,13 +14,15 @@ const Gadgets = () => {
   const macbook = gadgets.filter((p) => p.category === "macbook");
 
   return (
-    <div className="mt-20 ">
+    <div className="mt-20">
       <h1 className="text-3xl font-bold text-center ">
         Explore Cutting Edge Gadgets
       </h1>
 
-      <div className="flex flex-col gap-6 px-5  lg:flex-row justify-center pt-5 pb-10">
-        <div className="mt-6 px-8 md:px-4  h-fit py-5 mx-auto bg-white border-gray-100 rounded-xl ">
+      <div className="flex max-w-5xl mx-auto flex-col  gap-6 px-5  lg:flex-row pt-5 pb-10">
+
+        {/* left section */}
+        <div className="mt-6 px-8 mx-auto h-fit py-5 bg-white border-gray-100 rounded-xl ">
           <div className="flex flex-col gap-5">
             <Link
               to="/"
@@ -47,7 +49,7 @@ const Gadgets = () => {
               onClick={() => setTabIndex(3)}
               className="btn hover:bg-[#9538E2] hover:text-white rounded-3xl btn-soft bg-gray-50 text-gray-500"
             >
-              Smart Watches
+            Watches
             </Link>
             <button
               onClick={() => setTabIndex(4)}
@@ -58,9 +60,11 @@ const Gadgets = () => {
           </div>
         </div>
 
+
+
         {/* right section */}
         {tabIndex === 0 && (
-          <div className="grid mx-auto sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3 ">
+          <div className="grid mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {gadgets.map((singleGadget) => (
               <Gadget
                 key={singleGadget.product_id}
@@ -69,8 +73,9 @@ const Gadgets = () => {
             ))}
           </div>
         )}
+
         {tabIndex === 1 && (
-          <div className="grid mx-auto sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3 ">
+          <div className="grid mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {laptops.map((singleGadget) => (
               <Gadget
                 key={singleGadget.product_id}
@@ -81,7 +86,7 @@ const Gadgets = () => {
         )}
 
         {tabIndex === 2 && (
-          <div className="grid mx-auto sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3">
+          <div className="grid mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {phones.map((singleGadget) => (
               <Gadget
                 key={singleGadget.product_id}
@@ -92,7 +97,7 @@ const Gadgets = () => {
         )}
 
         {tabIndex === 3 && (
-          <div className="grid mx-auto sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3">
+          <div className="grid mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {watches.map((singleGadget) => (
               <Gadget
                 key={singleGadget.product_id}
@@ -103,7 +108,7 @@ const Gadgets = () => {
         )}
 
         {tabIndex === 4 && (
-          <div className="grid mx-auto sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-3">
+          <div className="grid mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {macbook.map((singleGadget) => (
               <Gadget
                 key={singleGadget.product_id}
